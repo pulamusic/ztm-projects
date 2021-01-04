30c44970a9bb8b7c886e839a828c901ff2612eae
Jim Carroll <jim@pulamusic.com>
2021-01-04 12:02:03 -0500

    moved cors-anywhere to Repositories

--- quote-generator/js/index.js
+++ quote-generator/js/index.js
@@ -1,1 +1,18 @@
-console.log('Wassaaaaaaaap?!')
+// get quote from api
+getQuote = async () => {
+  // proxyUrl provides a solution to common CORS issues
+  // **I need to duplicate this proxyUrl**
+  const proxyUrl = 'https://cors-anywhere.heroku.com/'
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+
+  try {
+    const response = await fetch(proxyUrl + apiUrl)
+    const data = response.json()
+    console.log(data)
+  } catch (error) {
+    console.log('Uuuuh...you fucked up somehow.', error)
+  }
+}
+
+// onload
+getQuote()
